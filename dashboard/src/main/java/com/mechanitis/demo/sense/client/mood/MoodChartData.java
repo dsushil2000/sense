7ebee5d95c41668bf02e26699024508af6bdad01
Trisha Gee <trisha.gee@gmail.com>
2016-07-29 11:50:49 +0200

    Functional interfaces should be annotated

--- dashboard/src/main/java/com/mechanitis/demo/sense/client/mood/MoodChartData.java
+++ dashboard/src/main/java/com/mechanitis/demo/sense/client/mood/MoodChartData.java
@@ -4,9 +4,12 @@ import com.mechanitis.demo.sense.infrastructure.MessageListener;
 import javafx.collections.ObservableList;
 import javafx.scene.chart.PieChart;
 
+import java.math.BigDecimal;
+import java.util.List;
+
 import static javafx.collections.FXCollections.observableArrayList;
 
-public class MoodChartData implements MessageListener<TweetMood> {
+public class MoodChartData implements MessageListener<Message> {
     private final PieChart.Data sadPortion = new PieChart.Data("Sad", 0);
     private final PieChart.Data happyPortion = new PieChart.Data("Happy", 0);
     private final PieChart.Data confusedPortion = new PieChart.Data("Errr...", 0);
@@ -15,9 +18,24 @@ public class MoodChartData implements MessageListener<TweetMood> {
     public ObservableList<PieChart.Data> getPieChartData() {
         return pieChartData;
     }
+    BigDecimal finalPrice;
+
+    private void callCalc() {
+        InventoryItem inventoryItem = new InventoryItem();
+        calc((item, activeDiscounts) -> {
+            BigDecimal price = item.getPrice();
+            finalPrice = price;
+            activeDiscounts.forEach(discount -> finalPrice = finalPrice.subtract(price.multiply(discount.getAsPercentage())));
+            return finalPrice;
+        });
+    }
+
+    private void calc(PriceCalculator priceCalculator){
+        priceCalculator.calculatePrice(null, null);
+    }
 
     @Override
-    public void onMessage(TweetMood message) {
+    public void onMessage(Message message) {
         incrementPie(message.isHappy(), () -> happyPortion.setPieValue(happyPortion.getPieValue() + 1));
         incrementPie(message.isSad(), () -> sadPortion.setPieValue(sadPortion.getPieValue() + 1));
         incrementPie(message.isConfused(), () -> confusedPortion.setPieValue(confusedPortion.getPieValue() + 1));
@@ -26,6 +44,29 @@ public class MoodChartData implements MessageListener<TweetMood> {
     private void incrementPie(boolean criteria, Runnable thingToDo) {
         if (criteria) {
             thingToDo.run();
+        }
+    }
+
+    interface PriceCalculator {
+        BigDecimal calculatePrice(InventoryItem item, List<Discount> activeDiscounts);
+    }
+
+
+
+
+    private static class Discount {
+        private BigDecimal asPercentage;
+
+        public BigDecimal getAsPercentage() {
+            return asPercentage;
+        }
+    }
+
+    private static class InventoryItem {
+        private BigDecimal price;
+
+        public BigDecimal getPrice() {
+            return price;
         }
     }
 }
